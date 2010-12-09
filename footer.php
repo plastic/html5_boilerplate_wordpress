0b298210a5d7ef37494cf3b9570e94487c720ef1
joel <joel.oliveira@gmail.com>
2010-12-09 15:31:57 -0500

    cleaning up extra options markup in functions.php. Also added a handful of new fields and options.

--- footer.php
+++ footer.php
@@ -1,7 +1,19 @@
 			<footer>
+				<?php if(show_footer_title()) { ?><h3><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h3><?php } ?>
 				<?php if(show_sidebar_at('footer')) { get_sidebar ('footer'); } ?>
-				<p class="right"><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><?php _e('<abbr title="Really Simple Syndication">RSS</abbr>'); ?></a> | <?php wp_register('', ' |'); ?> <?php wp_loginout(); ?></a> <?php wp_meta(); ?></p>
-				<p>&copy; Company Name</p>	
+				<p class="meta"> <a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><?php _e('<abbr title="Really Simple Syndication">RSS</abbr>'); ?></a> | <?php wp_register('', ' |'); ?> <?php wp_loginout(); ?></a> <?php wp_meta(); ?> </p>
+				<p class="copyright"> Copyright &copy; <?php echo date('Y').' '; ?> <?php echo get_option('company_name'); ?> All Rights Reserved </p>
+				<?php if(get_option('site_credit') !="" ) { ?><p class="credit"><?php echo get_option('site_credit'); ?></p><?php } ?>
+				<?php 
+					if(get_option('custom_menus') !="" ) 
+					{ 
+						$custom_menus = explode(",", strip_tags(get_option('custom_menus')));
+						foreach($custom_menus as $menu) 
+						{
+							wp_nav_menu( array('menu' => $menu) );
+						}
+					} ?>
+
 			</footer>
 		</div>
 		<?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 // ===================
 function show_sidebar_at($position) { return get_option('sidebar_'.$position) == "1" ? true : false; }
 function show_search_form() { return get_option('show_search') == "1" ? true : false; }
+function show_footer_title() { return get_option('show_footer_title') == "1" ? true : false; }
 
 // =========================
 // = CUSTOM THEME SETTINGS =
@@ -19,32 +20,107 @@ function editglobalcustomfields()
 	$sidebar_right_status = show_sidebar_at('right') == "1" ? "checked=\"yes\"" : "";
 	$sidebar_footer_status = show_sidebar_at('footer') == "1" ? "checked=\"yes\"" : "";
 	?>
+	<style type="text/css" media="screen">
+	
+		#theme_options label { font-weight:bold; font-size: 13px; display: block; }
+		#theme_options fieldset { border:solid 1px #ccc; padding: 0 10px; margin-top: 10px; margin-bottom: 20px;  }
+		#theme_options fieldset legend { 
+			padding: 0 10px; 
+			font: italic 16px/20px Georgia,"Times New Roman","Bitstream Charter",Times,serif;
+			color: #464646;
+		}
+		#theme_options .extra_info { display: block; font-size:11px; color:#999;}
+
+		#options_general_site_information strong {
+			display: block;
+			margin-bottom: 10px;
+		}
+		#options_general_site_information .sidebars input { float: left; margin-right:5px; }
+		#options_general_site_information .sidebars label {
+			clear: right;
+			display: block;
+			margin-bottom: 10px;
+		}
+	</style>
 	<div class='wrap'>
 		<h2>Theme Options</h2>
-  		<form method="post" action="options.php">
+  		<form method="post" action="options.php" id="theme_options">
 	  	  	<input type="hidden" name="action" value="update" />
-			<input type="hidden" name="page_options" value="sidebar_left,sidebar_right,sidebar_footer,show_search" />
+			<input type="hidden" name="page_options" value="company_name,site_credit,sidebar_left,sidebar_right,sidebar_footer,show_search,show_footer_title,custom_menus" />
 			<?php wp_nonce_field('update-options') ?>
 
-			<p>
-				<strong>Which sidebars would you like to see?</strong><br />
-				<input type="checkbox" name="sidebar_left" value="1" id="sidebar_left" <?php echo $sidebar_left_status ?> /> Left Sidebar<br />
-				<input type="checkbox" name="sidebar_right" value="1" id="sidebar_right" <?php echo $sidebar_right_status ?> /> Right Sidebar<br />
-				<input type="checkbox" name="sidebar_footer" value="1" id="sidebar_footer" <?php echo $sidebar_footer_status ?> /> Footer Sidebar
-			</p>
 
-			<p>
-				<strong>Show search form in the header?</strong><br />
-				<select name="show_search" id="show_search">
-				<?php if (show_search_form()) : ?>
-					<option value="1" selected="selected">Yes</option>
-					<option value="0">No</option>
-				<?php else : ?>
-					<option value="1">Yes</option>
-					<option value="0" selected="selected">No</option>
-				<?php endif; ?>
-				</select>
-			</p>
+			<fieldset id="options_general_site_information" class="">
+				<legend>General Site Information</legend>
+				
+				<p>
+					<label for="company_name">Your company name</label>
+					<input type="text" name="company_name" value="<?php echo htmlspecialchars(get_option('company_name')); ?>" id="company_name" size="40" />
+				</p>
+
+				<p>
+					<label for="company_name">Site Credit</label>
+					<input type="text" name="site_credit" value="<?php echo htmlspecialchars(get_option('site_credit')); ?>" id="site_credit" size="40" />
+				</p>
+
+
+				<p class="sidebars">
+					<strong>Which sidebars would you like to see?</strong>
+
+					<input type="checkbox" name="sidebar_left" value="1" id="sidebar_left" <?php echo $sidebar_left_status ?> /> <label for="sidebar_left">Left Sidebar</label>
+
+					<input type="checkbox" name="sidebar_right" value="1" id="sidebar_right" <?php echo $sidebar_right_status ?> /> <label for="sidebar_right">Right Sidebar</label>
+
+					<input type="checkbox" name="sidebar_footer" value="1" id="sidebar_footer" <?php echo $sidebar_footer_status ?> /> <label for="sidebar_footer">Footer Sidebar</label>
+				</p>
+				
+			</fieldset>
+
+			<fieldset id="options_header" class="">
+				<legend>Header</legend>
+				
+				<p>
+					<label for="">Show search form in the header?</label>
+					<select name="show_search" id="show_search">
+					<?php if (show_search_form()) : ?>
+						<option value="1" selected="selected">Yes</option>
+						<option value="0">No</option>
+					<?php else : ?>
+						<option value="1">Yes</option>
+						<option value="0" selected="selected">No</option>
+					<?php endif; ?>
+					</select>
+				</p>
+				
+			</fieldset>
+
+			<fieldset id="options_footer" class="">
+				<legend>Footer</legend>
+				
+				<p>
+					<label for="">Show website title in footer?</label>
+					<select name="show_footer_title" id="show_footer_title">
+						<?php if (show_footer_title()) : ?>
+							<option value="1" selected="selected">Yes</option>
+							<option value="0">No</option>
+						<?php else : ?>
+							<option value="1">Yes</option>
+							<option value="0" selected="selected">No</option>
+						<?php endif; ?>
+					</select>
+				</p>
+				
+				<p>
+					<label for="custom_menus_to_include">Custom Menus to Include</label>
+					<span class="extra_info">
+						Place the titles of the custom menus in the following form field delimited by commas.  
+						Menus will then be included in the theme footer.
+					</span>
+					<input type="text" name="custom_menus" value="<?php echo strip_tags(get_option('custom_menus')); ?>" id="custom_menus" size="40" />
+					
+				</p>
+				
+			</fieldset>
 
 			<p><input type="submit" name="Submit" value="Update Options" /></p>
 
